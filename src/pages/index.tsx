e2373aaa1b6d7cc7929c435c2d2921db161fbb69
emalineg <mgayhart0@ivc.edu>
2022-11-16 23:16:21 -0800

    added fonts to public

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,14 @@ import { trpc } from "../utils/trpc";
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
 import FeaturedTeams from "../components/FeaturedTeams";
+import localFont from "@next/font/local";
 
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
+  const montliFont = localFont({ src: "/fonts/Mont-ExtraLightDEMO.otf" });
+  const montheFont = localFont({ src: "/fonts/Mont-HeavyDEMO.otf" });
+  const theboldFont = localFont({ src: "/fonts/THEBOLDFONT.otf" });
+  const nextFont = localFont({ src: "/fonts/Next.otf" });
   return (
     <>
       <Head>
@@ -21,13 +25,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
-        className="bg-cover w-screen"
+        className="w-screen bg-cover"
         style={{ backgroundImage: `url('/bg.svg')` }}
       >
         <Nav />
         <Banner />
         <FeaturedTeams />
-
       </div>
     </>
   );
