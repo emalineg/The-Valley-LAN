bb10e5861429b2f64a3cf374da80e8041915e9d7
emalineg <mgayhart0@ivc.edu>
2022-11-16 23:05:12 -0800

    fixed background

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,7 +10,7 @@ export default function Banner() {
 
           <div className="mt-5 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+              className="block w-full rounded border border-green-600 px-12 py-3 text-sm font-medium text-white hover:bg-green-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
               href="#rsvp"
             >
               Spectate IRL
