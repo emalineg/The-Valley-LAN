135b83572c49b89d05c61b19e864acb559b66675
emalineg <mgayhart0@ivc.edu>
2022-11-17 06:08:40 -0800

    added more favicons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,24 @@ const Home: NextPage = () => {
           name="description"
           content="A Rocket League Championship by Irvine Valley College"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <div
         className="w-screen bg-cover"
