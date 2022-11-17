5f2d78b4d51bb06e2280cf51ba0bc967d2c5c7ce
cloudnothings <carlos@rangel.us>
2022-11-16 22:14:52 -0800

    Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
+      <Banner />
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <Banner />
+
         <div>
           <FeaturedTeams />
         </div>
