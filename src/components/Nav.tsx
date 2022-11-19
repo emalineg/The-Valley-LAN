2bc717168f59724ef0a5ece626594bad0fca0e35
emalineg <mgayhart0@ivc.edu>
2022-11-19 01:45:19 -0800

    nav bar responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,65 +2,70 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav
-      aria-label="Site Nav"
-      className="mx-auto flex items-center justify-between bg-black/60 "
-    >
-      <Link
-        href="/"
-        className="ml-20 inline-flex h-20 w-20 items-center justify-center rounded-lg bg-transparent"
-      >
-        <span className="sr-only">Logo</span>
-        <img src="FinalWordmark.svg" alt="The Valley Finals Logo" />
-      </Link>
-
-      <ul
-        style={{
-          fontFamily: "MontLI",
-          color: "white",
-        }}
-        className=" mr-10 flex items-center gap-2 text-sm font-medium"
-      >
-        <li>
-          <Link
-            className="rounded-lg px-3 py-2 hover:text-green-400"
-            href="https://twitch.tv/ivc_esports"
-          >
-            {" "}
-            Twitch{" "}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className=" rounded-lg px-3 py-2 hover:text-green-400"
-            href="https://www.communitygaming.io/tournament/the-valley-a-rocket-league-championship"
-            target="_blank"
-          >
-            {" "}
-            Brackets{" "}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="rounded-lg px-3 py-2 hover:text-green-400"
-            href="https://forms.gle/eWdnhABkQwTjZGpy7"
-            target="_blank"
-          >
-            {" "}
-            RSVP{" "}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="rounded-lg px-3 py-2 hover:text-green-400"
-            href="#sponsors"
-            scroll={false}
-          >
-            {" "}
-            Partners{" "}
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div aria-label="Site Nav" className=" bg-black/60 px-6 sm:px-6 lg:px-8">
+      <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex grid basis-full grid-cols-2">
+          <div>
+            <Link href="/">
+              <span className="sr-only">Logo</span>
+              <img
+                src="tv.png"
+                alt="The Valley Finals Logo"
+                className="h-auto w-16"
+              />
+            </Link>
+          </div>
+          <div className="flex justify-end">
+            <ul
+              style={{
+                fontFamily: "MontLI",
+                color: "white",
+              }}
+              className="flex items-center text-sm font-medium"
+            >
+              <li>
+                <Link
+                  className="rounded-lg px-3 py-2 hover:text-green-400"
+                  href="https://twitch.tv/ivc_esports"
+                >
+                  {" "}
+                  Twitch{" "}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className=" rounded-lg px-3 py-2 hover:text-green-400"
+                  href="https://www.communitygaming.io/tournament/the-valley-a-rocket-league-championship"
+                  target="_blank"
+                >
+                  {" "}
+                  Brackets{" "}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="rounded-lg px-3 py-2 hover:text-green-400"
+                  href="https://forms.gle/eWdnhABkQwTjZGpy7"
+                  target="_blank"
+                >
+                  {" "}
+                  RSVP{" "}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="rounded-lg px-3 py-2 hover:text-green-400"
+                  href="#sponsors"
+                  scroll={false}
+                >
+                  {" "}
+                  Partners{" "}
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
